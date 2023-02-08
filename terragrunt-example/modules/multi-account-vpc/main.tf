aa51f2af9786707e513b4a1ff9fc9816a547fb6d
Ian <101607944+iangrunt@users.noreply.github.com>
2023-02-07 20:36:28 -0500

    Update main.tf

--- terragrunt-example/modules/multi-account-vpc/main.tf
+++ terragrunt-example/modules/multi-account-vpc/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = "~> 4.0"
+      configuration_aliases = [aws.account_a, aws.account_b]
+    }
+  }
+}
+
 module "vpc_a" {
   source = "git@github.com:iangrunt/terraform-fake-modules.git//modules/aws/vpc?ref=main"
 
